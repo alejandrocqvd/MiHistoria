193bb0cc9b6b033da6954d800a93dc7f9a34bf7c
alejandrocqvd <alejandrocqvd@gmail.com>
2023-12-15 12:42:10 -0700

    Updated footer design

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -4,18 +4,18 @@ export const Footer = () => {
   return (
     <>
       <div className='flex flex-col justify-center items-center bottom-0 h-auto w-screen m-0 p-24 bg-secondary'>
-        <div className='flex flex-col md:flex-row md:justify-between w-9/12 items-center p-4'>
-          <div className='flex flex-col text-center md:text-left'>
+        <div className='flex flex-col md:flex-row md:justify-between w-9/12 items-start p-4'>
+          <div className='flex-1 flex flex-col text-center md:text-left'>
             <h1 className='font-bold text-xl'>Discover MiHistoria</h1>
-            <p>Explore stuff lol</p>
+            <p>Take time to look back, reflect, and write your story. Explore our features and the broad collection of stories.</p>
           </div>
-          <div className='flex flex-col text-center md:text-left my-8 md:my-0'>
+          <div className='flex-1 flex flex-col text-center md:text-left mx-8 my-8 md:my-0'>
             <h1 className='font-bold text-xl'>Explore Stories</h1>
-            <p>Explore stuff lol</p>
+            <p>Check our top stories or search for a story that intrigues you. Like, share, save, and comment on your favorites.</p>
           </div>
-          <div className='flex flex-col text-center md:text-left'>
+          <div className='flex-1 flex flex-col text-center md:text-left'>
             <h1 className='font-bold text-xl'>Get Inspired</h1>
-            <p>Explore stuff lol</p>
+            <p>Check our help page to assist you in getting started on your story writing journey.</p>
           </div>
         </div>
         <hr className='bg-white h-1/2 w-9/12 my-8'></hr>
