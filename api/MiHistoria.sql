21b5f82ef7d3d6c3aff21a5cae091b0426273c79
alejandrocqvd <alejandrocqvd@gmail.com>
2023-12-19 14:39:49 -0700

    Updated SQL to include pages

--- api/MiHistoria.sql
+++ api/MiHistoria.sql
@@ -4,72 +4,70 @@ USE mi_historia;
 
 CREATE TABLE user
 (
-    username             INT             NOT NULL            AUTO_INCREMENT,
+    username            INT             NOT NULL            AUTO_INCREMENT,
     first_name          VARCHAR(70),
-    middle_name         VARCHAR(70),
     last_name           VARCHAR(70),
     dob                 DATE,
-    email               VARCHAR(255),
-    password            VARCHAR(255),
+    email               VARCHAR(255)    NOT NULL,
+    password            VARCHAR(255)    NOT NULL,
     image               VARCHAR(255),
-    private             BOOLEAN         DEFAULT FALSE,
+    is_private          BOOLEAN         DEFAULT FALSE,
     PRIMARY KEY (username)
 );
 
-CREATE TABLE post 
+CREATE TABLE story
 (
-    post_id             INT             NOT NULL            AUTO_INCREMENT,
-    username             INT            NOT NULL,
-    title               VARCHAR(255)    NOT NULL
-    text                MEDIUMTEXT      NOT NULL,
+    story_id            INT             NOT NULL            AUTO_INCREMENT,
+    username            INT             NOT NULL,
+    title               VARCHAR(255)    NOT NULL,
     timestamp           DATETIME        NOT NULL,
-    PRIMARY KEY (post_id),
-    FOREIGN KEY (username) REFERENCES User(username)
+    PRIMARY KEY (story_id),
+    FOREIGN KEY (username) REFERENCES user(username)
 );
 
-CREATE TABLE post_media
+CREATE TABLE page
 (
-    post_id             INT             NOT NULL,
-    media               VARCHAR(255)    NOT NULL,
-    PRIMARY KEY (post_id, media),
-    FOREIGN KEY (post_id) REFERENCES Post(post_id)
+    page_number         INT             NOT NULL            AUTO_INCREMENT,
+    story_id            INT             NOT NULL,
+    text                MEDIUMTEXT,
+    PRIMARY KEY (page_number, story_id),
+    FOREIGN KEY (story_id) REFERENCES story(story_id)
 );
 
 CREATE TABLE comment
 (
     comment_id          INT             NOT NULL            AUTO_INCREMENT,
-    username             INT             NOT NULL,
-    post_id             INT             NOT NULL,
+    username            INT             NOT NULL,
+    story_id            INT             NOT NULL,
     text                TEXT            NOT NULL,
     timestamp           DATETIME        NOT NULL,
-    type                VARCHAR(50)     NOT NULL            DEFAULT 'text',
     PRIMARY KEY (comment_id),
-    FOREIGN KEY (username) REFERENCES User(username),
-    FOREIGN KEY (post_id) REFERENCES Post(post_id)
+    FOREIGN KEY (username) REFERENCES user(username),
+    FOREIGN KEY (story_id) REFERENCES story(story_id)
 );
 
-CREATE TABLE likes_post
+CREATE TABLE likes_story
 (
     username             INT             NOT NULL,
-    post_id             INT             NOT NULL,
-    PRIMARY KEY (username, post_id),
-    FOREIGN KEY (username) REFERENCES User(username),
-    FOREIGN KEY (post_id) REFERENCES Post(post_id)
+    story_id             INT             NOT NULL,
+    PRIMARY KEY (username, story_id),
+    FOREIGN KEY (username) REFERENCES user(username),
+    FOREIGN KEY (story_id) REFERENCES story(story_id)
 );
 
 CREATE TABLE saves
 (
     username             INT             NOT NULL,
-    post_id             INT             NOT NULL,
-    timestamp           DATETIME        NOT NULL,
-    PRIMARY KEY (username, post_id),
-    FOREIGN KEY (username) REFERENCES User(username),
-    FOREIGN KEY (post_id) REFERENCES Post(post_id)
+    story_id             INT             NOT NULL,
+    timestamp            DATETIME        NOT NULL,
+    PRIMARY KEY (username, story_id),
+    FOREIGN KEY (username) REFERENCES user(username),
+    FOREIGN KEY (story_id) REFERENCES story(story_id)
 );
 
 CREATE TABLE likes_comment
 (
-    username             INT             NOT NULL,
+    username            INT             NOT NULL,
     comment_id          INT             NOT NULL,
     PRIMARY KEY (username, comment_id),
     FOREIGN KEY (username) REFERENCES User(username),
